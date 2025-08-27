36217b510b750315f791a59ba05a4f370bc21f02
Aurum Linh <aurum.linh@gmail.com>
2025-08-26 22:20:31 -0700

    rotating title experiment

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+function changeTitles(titles, interval) {
+    let currentIndex = 0;
+
+    function changeTitle() {
+        document.title = titles[currentIndex];
+        currentIndex = (currentIndex + 1) % titles.length;
+        setTimeout(changeTitle, interval);
+    }
+
+    changeTitle();
+}
+
+
+const titles = [" aurum linh 𓆩🖤𓆪 ", "⋆˚₊ 𖤓☽˚.⋆", "°‧ 𓆝 𓆟 𓆞 ·｡"];
+const interval = 2000; // 2 seconds
+
+changeTitles(titles, interval);
